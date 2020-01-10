0a68fbfd4fc63d3e0d06c0580f20c071d752100b
LaupWing <loc_strict@hotmail.com>
2020-01-10 16:07:50 +0100

    cleaned out defautl vue

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,16 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+
+    },
+    mutations: {
+        
+    },
+    actions: {
+        
+    },
+    modules: {
+        
+    }
 })
