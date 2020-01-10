22654cd604f94d35c3546aefc84f7d97a211920c
LaupWing <loc_strict@hotmail.com>
2020-01-10 16:32:03 +0100

    added button ui component

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,12 @@
     margin: 0;
     padding:0;
     box-sizing: border-box;
+    --turqoise: #56AAAA;
+    --lighterGrey:#fffaff;
+    --lightGrey: #C3C6CB;
+    --mediumGrey:#ced1d6;
+    --darkGrey: #86898E;
+    --justWhite: white;
+    --justBlack: black;
 }
 </style>
---
+++ src/components/UI/Buttons/Button.vue
@@ -0,0 +1,32 @@
+<template>
+    <button 
+        class="button"
+        :class='className'
+        :style={styleObj}
+    >
+        {{content}}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props:{
+        className:{
+            required: true,
+            type: String
+        },
+        content:{
+            type: String,
+            required: true
+        },
+        styleObj:{
+            type: Object
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
