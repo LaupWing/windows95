38cf3f1bc68a3568b3b5842c6eb1a088790261e7
LaupWing <loc_strict@hotmail.com>
2020-01-15 12:22:33 +0100

    added a svg more

--- src/components/UI/MenuItem/MenuItem.vue
+++ src/components/UI/MenuItem/MenuItem.vue
@@ -1,7 +1,16 @@
 <template>
     <li class="item" :style="extraStyle">
         <img :src="require(`${icon}`)" alt="">
-        <h2>{{title}}</h2>
+        <h2>
+            {{title}}
+            <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                viewBox="0 0 141 239"
+                v-if="expandable"
+            >    
+                <polygon points="141 119.5 -0.22 0.47 -0.22 238.53 141 119.5"/>
+            </svg> 
+        </h2>
     </li>
 </template>
 
