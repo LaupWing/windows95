4b1ba044ff45cf0c8eb923c941c950f217246137
LaupWing <loc_strict@hotmail.com>
2020-01-15 12:14:40 +0100

    added base menuitem props

---
+++ src/components/UI/MenuItem/MenuItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <li class="item" :style="extraStyle">
+        <img :src="require(`${icon}`)" alt="">
+        <h2>{{title}}</h2>
+    </li>
+</template>
+
+<script>
+export default {
+    name: 'ListItem',
+    props:{
+        icon:{
+            type: String,
+            required: true
+        },
+        title:{
+            type: String,
+            required: true
+        },
+        extraStyle:{
+            type: Object
+        },
+        expandable:{
+            type: Boolean,
+            required: true
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
